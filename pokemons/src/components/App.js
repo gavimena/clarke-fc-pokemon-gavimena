cacde7fb894a50b3954de2d40393e96903438a7f
Gabriela Mena <gabriela.grojas@gmail.com>
2018-03-10 10:36:49 +0100

    get data and add background color

--- pokemons/src/components/App.js
+++ pokemons/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+//import Background from './images/pikachuBkg.png';
 
 class App extends Component {
   constructor(props) {
@@ -9,26 +10,43 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    fetch('https://pokeapi.co/api/v2/pokemon/1/')
-    .then(response => response.json())
-    .then(json => {
-      this.setState({
-        pokemons: json
-      });
-    });
-  }
+  // componentDidMount() {
+  //   fetch('https://pokeapi.co/api/v2/pokemon/1/')
+  //   .then(response => response.json())
+  //   .then(json => {
+  //     this.setState({
+  //       pokemons: json
+  //     });
+  //    console.log('fetch',this.state.pokemons);
+  //   });
+  // }
 
   render() {
+
+    // let types = this.state.pokemons.types;
+    // console.log(types);
+    // console.log(this.state.pokemons.sprites);
+
+
+    // <li> {this.state.pokemons.name} </li>
+    // <li> {this.state.pokemons.id} </li>
+    // <li> <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" /></li>
+    // <li> {this.state.pokemons.types[0].name} </li>
+    // <img className="App__background" src={Background} alt="background" />
     return (
       <div className="App">
+
         <header>
           <h1>Pokemons</h1>
           <input className="input" type="text" name="search" placeholder="Write a Pokemon's character" />
         </header>
         <main>
           <ul>
-            <li> {this.state.pokemons.name} </li>
+            <li> Bulbasaur</li>
+            <li> 1 </li>
+            <li> <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="pokemon" /></li>
+            <li> poison </li>
+            <li> grass </li>
           </ul>
         </main>
       </div>
