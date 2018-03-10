539ec8b3ad39bd8687d413ab198af93bf6a4d30a
Gabriela Mena <gabriela.grojas@gmail.com>
2018-03-10 18:57:09 +0100

    add styles and media queries

--- pokemons/src/components/PokemonList.js
+++ pokemons/src/components/PokemonList.js
@@ -5,13 +5,13 @@ class PokemonList extends React.Component {
   render() {
     let pokemons = this.props.pokemons;
     let imageUrlBase =  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
-    //let id = urlImages.charAt(urlImages.length-5)
+
     return(
-      <div>
+      <div className="pokemon__card">
         {pokemons.map(pokemon => {
-          return <div key={pokemon.id} className="pokemon__container-front">
-            <h3>{pokemon.name} - {pokemon.id} </h3>
-            <img className="pokemon__image" src={`${imageUrlBase}${pokemon.id}.png`} />
+          return <div key={pokemon.id} className="pokemon__card-front">
+            <h3 className="pokemon__name">{pokemon.name} - {pokemon.id} </h3>
+            <img className="pokemon__image" src={`${imageUrlBase}${pokemon.id}.png`} alt="pokemon" />
             <button className="info-btn">More Info</button>
           </div>
         })}
