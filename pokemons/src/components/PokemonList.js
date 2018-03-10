4cafdb82cea678160911d3429022a85c72d0a2c6
Gabriela Mena <gabriela.grojas@gmail.com>
2018-03-10 23:03:37 +0100

    get more details from pokemon API

--- pokemons/src/components/PokemonList.js
+++ pokemons/src/components/PokemonList.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 class PokemonList extends React.Component {
 
+  renderDetails(selectedPokemon){
+    return (
+      <div className="pokemon__card-back">
+          <h3>Types</h3>
+          <ul>
+            <li>Base experience: {selectedPokemon.base_experience}</li>
+          </ul>
+      </div>
+    );
+  }
+
   render() {
     let pokemons = this.props.pokemons;
     let imageUrlBase =  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+    let selectedPokemon = this.props.selectedPokemon || {id:0};
 
     return(
       <div className="pokemon__card">
@@ -12,15 +24,11 @@ class PokemonList extends React.Component {
           return <div key={pokemon.id} className="pokemon__card-front">
             <h3 className="pokemon__name">{pokemon.name} - {pokemon.id} </h3>
             <img className="pokemon__image" src={`${imageUrlBase}${pokemon.id}.png`} alt="pokemon" />
-            <button className="info-btn">More Info</button>
+            {(pokemon.id === selectedPokemon.id) ?
+                this.renderDetails(selectedPokemon):''}
+            <button className="info-btn" onClick={() => {this.props.onMoreInfo(pokemon.id)}}>More Info</button>
           </div>
         })}
-        <div className="pokemon__card-back">
-          <h3>Types</h3>
-          <ul>
-            <li></li>
-          </ul>
-        </div>
       </div>
     );
   }
